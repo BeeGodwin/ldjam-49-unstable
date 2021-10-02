71c47b184052856d17611692b40ae70c060f6e81
Bee Godwin <bee.godwin@bbc.co.uk>
2021-10-02 13:06:17 +0100

    expand jump state machine

--- Assets/Bee/Player/PlayerController.cs
+++ Assets/Bee/Player/PlayerController.cs
@@ -3,28 +3,28 @@ using UnityEngine;
 
 namespace Bee.Player
 {
-    public class PlayerController : MonoBehaviour
+    public class PlayerController : MonoBehaviour, IJumper
     {
-    
-        // make player state machine
-        
         // components
         private Rigidbody2D rb;
-        private PlayerJumpStateMachine state;
+        private PlayerJumpStateMachine stateMachine;
         
         // parameters
         public float maxSpeed;
         public float accel;
         public float jumpForce;
+        public float airWalkDamping;
 
         void Start()
         {
             rb = this.gameObject.transform.GetChild(0).GetComponent<Rigidbody2D>();
-            state = new PlayerJumpStateMachine();
+            stateMachine = new PlayerJumpStateMachine();
         }
 
         void Update()
         {
+            stateMachine.Update(this);
+            
             var xAxis = Input.GetAxis("Horizontal");
             var jump = Input.GetButton("Jump");
             
@@ -37,22 +37,31 @@ namespace Bee.Player
 
         private void HandleJump()
         {
-            // consult state machine
-            // changes velocity
-            rb.AddForce(new Vector2(0, jumpForce));
+            stateMachine.tryJump(this, jumpForce);
         }
 
         private void HandleMove(float xAxis)
         {
-            // consult state machine
             // flip player if needed
+
+            var moveForce = stateMachine.GetState() == JumpState.Grounded ? jumpForce : jumpForce * airWalkDamping;
             
-            rb.AddForce(new Vector2(xAxis, 0) * accel); // should this be relative to the raft?
+            rb.AddForce(new Vector2(xAxis, 0) * moveForce); // should this be relative to the raft?
             if (rb.velocity.magnitude > maxSpeed)
             {
                 var cappedVelocity = rb.velocity.normalized * maxSpeed;
                 rb.velocity = cappedVelocity;
             }
+        }
+
+        public bool IsGrounded()
+        {
+            return true;
+        }
+
+        public void doJump(float withJumpForce)
+        {
+            rb.AddForce(new Vector2(0, withJumpForce));
         }
     }
 }
--- Assets/Bee/Player/PlayerJumpStateMachine.cs
+++ Assets/Bee/Player/PlayerJumpStateMachine.cs
@@ -15,11 +15,39 @@ namespace Bee.Player
         {
             return _jumpState;
         }
+
+        public void tryJump(IJumper player, float jumpForce)
+        {
+            if (_jumpState == JumpState.Grounded && player.IsGrounded())
+            {
+                player.doJump(jumpForce);
+                _jumpState = JumpState.Airborne;
+            }
+        }
+
+        public void Update(IJumper player)
+        {
+            switch (_jumpState)
+            {
+                case JumpState.Airborne:
+                    if (player.IsGrounded())
+                    {
+                        _jumpState = JumpState.Grounded;
+                    }
+                    break;
+                case JumpState.Grounded:
+                    //noop
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 
     public interface IJumper
     {
         public bool IsGrounded();
+        public void doJump(float withJumpForce);
     }
 
     public enum JumpState
