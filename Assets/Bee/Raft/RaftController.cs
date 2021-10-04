d7dbe95823169851508021de4fd78f0efa47a558
Bee Godwin <bee.godwin@bbc.co.uk>
2021-10-04 15:06:03 +0100

    raft pauses nicely. although everything falls through it when it does

--- Assets/Bee/Raft/RaftController.cs
+++ Assets/Bee/Raft/RaftController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Bee.Game;
 using Bee.Sky;
 using UnityEngine;
@@ -6,26 +7,31 @@ namespace Bee.Raft
 {
     public class RaftController : MonoBehaviour, IBlowable, IGameSystem
     {
-        private Rigidbody2D _mastRb;
+        private List<Rigidbody2D> _rbs;
 
         void Start()
         {
-            _mastRb = GetComponentInChildren<Rigidbody2D>();
+            _rbs = new List<Rigidbody2D>
+            {
+                GameObject.Find("Mast").GetComponent<Rigidbody2D>(),
+                GameObject.Find("RaftBody").GetComponent<Rigidbody2D>()
+            };
+            _rbs.ForEach(rb => rb.simulated = false);
         }
         
         public void ApplyWindForce(float force)
         {
-            _mastRb.AddForce(Vector2.right * force);
+            _rbs[0].AddForce(Vector2.right * force);
         }
 
         public void PlayGame()
         {
-            _mastRb.constraints = RigidbodyConstraints2D.None;
+            _rbs.ForEach(rb => rb.simulated = true);
         }
 
         public void PauseGame()
         {
-            _mastRb.constraints = RigidbodyConstraints2D.FreezeAll;
+            _rbs.ForEach(rb => rb.simulated = false);
         }
 
         public void ResetGame()
